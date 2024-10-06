ddaac4042a1fb561da907137a8f571e708453495
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-10-07 01:05:15 +0300

    add hover for btn form and card

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,8 +12,8 @@ const Contact = ({ user }) => {
   return (
     <>
       <div>
-        <p className={s.contactText}>{user.name}</p>
-        <p className={s.contactText}>{user.number}</p>
+        <p className={s.contactText}>Name: {user.name}</p>
+        <p className={s.contactText}>Phone: {user.number}</p>
       </div>
       <button
         className={s.contactBtn}
